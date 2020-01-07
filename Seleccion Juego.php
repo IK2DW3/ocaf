048bd0de315f7d401f4981605315fe8f079a1ed5
Lucian <Lucian2302@gmail.com>
2020-01-07 13:22:29 +0100

    hola dani

---
+++ Seleccion Juego.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css.css">
+</head>
+    <title>Seleccion de Juego</title>
+</head>
+<body>
+
+<header> MEGA SELECT DE JUEGO</header>
+
+<section>
+
+<div id ="jugador1"><img src="dado.png"  alt=""><input type="text" placeholder="nombre"></div>
+<div id ="jugador2"><img src="dado.png"  alt=""><input type="text" placeholder="nombre"></div>
+<div id ="jugador3"><img src="dado.png"  alt=""><input type="text" placeholder="nombre"></div>
+<div id ="jugador4"><img src="dado.png"  alt=""><input type="text" placeholder="nombre"></div>
+
+</section>
+
+
+
+<footer></footer>
+    
+</body>
+</html>
