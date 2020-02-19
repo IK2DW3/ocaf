18effe10e6698b530bd52acd257f2550707cb166
Daniel Mosquera <demg.working@gmail.com>
2020-02-19 13:32:03 +0100

    Blog 51%

--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -4,17 +4,18 @@
 
 
     <div class="contenedor">
-        <div class="row align-items-center justify-content-center contenido mx-2 my-2">
-            <div class="col-sm-8 blog-body">
+        <div class="row align-items-start justify-content-center contenido blog-body mx-2 my-2">
+            <div class="col-sm-8 blog-section">
                 <div class="row">
                     <div class="col-sm-12"><h2>{{$arrayPost->titulo}}</h2></div>
                     <div class="col-sm-12"><p>{{$arrayPost->descripcion}}</p></div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-8 blog-section">
-                        <div class="row">
-                            <h3>Ultimas publicaciones</h3>
-                        </div>
+                    <div class="col-sm-12">
+                        <h3>Ultimas publicaciones</h3>
+                    </div>
+                    <div class="col-sm-12 blog-article">
+                        
                     </div>
                 </div>
             </div>
